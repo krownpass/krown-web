f338c0da4d97fd1a2cc6d01e362f1acd7dd3d9b8
Kaarthik-07 <57kaarthikj@gmail.com>
2025-11-02 17:40:36 +0530

    [feat]-> deploy

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,16 +2,17 @@ import axios from "axios";
 import { getToken } from "./auth";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000/api",
+    // baseURL: "https://krown-server.onrender.com/api",
+    baseURL: 'http://localhost:4000/api',
 });
 
 // Automatically inject token before every request
 api.interceptors.request.use((config) => {
-  const token = getToken(); // from localStorage
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
+    const token = getToken(); // from localStorage
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
 });
 
 export default api;
