ff82784f9bde3df38436b981745303424e05b60c
sahilkohli592 <115031333+sahilkohli592@users.noreply.github.com>
2025-11-24 13:42:22 +0530

    Update api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 import { getToken } from "./auth";
 
 const api = axios.create({
-    // baseURL: "https://krown-server.onrender.com/api",
-    baseURL: 'http://localhost:4000/api',
+    baseURL: `${process.env.NEXT_PUBLIC_BACKEND_API}/api`,
+    //baseURL: 'http://localhost:4000/api',
 });
 
 // Automatically inject token before every request
@@ -16,3 +16,4 @@ api.interceptors.request.use((config) => {
 });
 
 export default api;
+
